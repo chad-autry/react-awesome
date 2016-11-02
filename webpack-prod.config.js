ab09b05089fbfcd6537bb3e9d4951afd9834248a
Chad Autry <chad.autry@gmail.com>
2016-11-01 22:09:16 -0500

    Begin the switch to webpack from browserify

---
+++ webpack-prod.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+
+
+var prodLoaders = [
+  // javascript/jsx loader - https://www.npmjs.com/package/babel-loader - without the react-hot loader
+  {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    loaders: ['babel-loader'],
+  }
+]
+
+module.exports = {
+  entry: [
+  // our entry file
+  './src/bootStrap.js'
+  ],
+  output: {
+    path: './target/webapp',
+    filename: 'bundle.js'
+  },
+  devtool:'source-map',
+  module: {
+    loaders: prodLoaders
+  },
+  plugins: [
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.UglifyJsPlugin({minimize: true})
+  ],
+};
