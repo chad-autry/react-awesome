1497ad50016988bcfcd4b289669c39c02d73e2df
Chad Autry <chad.autry@gmail.com>
2016-11-03 08:07:55 -0500

    Add travisCI deployment

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ev;
+cd target/webapp
+git init
+git config user.name ${GH_NAME}
+git config user.email ${GH_EMAIL}
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
