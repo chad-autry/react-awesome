5565d04f4042ef1b69982a4745f6216c81c892bc
Chad Autry <chad.autry@gmail.com>
2017-07-28 08:15:29 -0500

    Update Home.jsx
    Relative xlinkHref

--- gh-pages/src/components/Home.jsx
+++ gh-pages/src/components/Home.jsx
@@ -33,8 +33,8 @@ module.exports = class Home extends React.Component {
                 </div>
             
                 <div className="marketing">
-                  <Icon width="42" height="42" href="sprites.svg#arrow-down"/>
-                  <Icon width="42" height="42" href="sprites.svg#trophy">
+                  <Icon width="42" height="42" href="/sprites.svg#arrow-down"/>
+                  <Icon width="42" height="42" href="/sprites.svg#trophy">
                       <Icon width="42" height="42" transform="translate(0 -5) scale(.25) rotate(180)" href="sprites.svg#arrow-down" isCutout={true}/>
                   </Icon>
                 </div>
